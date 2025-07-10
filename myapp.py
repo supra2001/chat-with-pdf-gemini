44d88a72c2bba4b63c3745509abdd77a0c9f1e29
Supratim Mukherjee <supratim.2127@gmail.com>
2025-07-10 21:42:30 +0530

    final

--- myapp.py
+++ myapp.py
@@ -4,7 +4,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.prompts import PromptTemplate
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 import os
 from dotenv import load_dotenv
